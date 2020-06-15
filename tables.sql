7c41503280c40c38f527f4756ffcc74dad211bcd
ReadyHash <Readyhash@gmail.com>
2020-06-15 10:09:22 +0800

    minor fix to tables

--- tables.sql
+++ tables.sql
@@ -1,15 +1,15 @@
-CREATE TABLE stores (
+CREATE TABLE IF NOT EXISTS stores (
     id SERIAL PRIMARY KEY,
     name TEXT
 );
 
-CREATE TABLE foods (
+CREATE TABLE IF NOT EXISTS foods (
 		id SERIAL PRIMARY KEY,
 		store_id INTEGER,
 		name TEXT
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 		id SERIAL PRIMARY KEY,
 		name TEXT,
 		email TEXT,
